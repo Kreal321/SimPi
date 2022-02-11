77d60ffa41666834b16752488c7f06c2c5f3ed8d
kreal <xukzhen@hotmail.com>
2022-02-11 17:18:42 -0500

    test communication

--- Server/simpi.py
+++ Server/simpi.py
@@ -7,12 +7,11 @@ class Option:
     def __init__(self) -> None:
         pass
 
-    def suspend(self):
-        while(True):
+    def waitUntil(self, signals, idx):
+        signals[idx] = True
+        while(signals[idx]):
+            print(f"Simpi is waiting signal")
             time.sleep(1)
-    
-    def resume(self):
-        pass
 
     def sleep(self, min:int):
         time.sleep(min)
@@ -21,7 +20,10 @@ def simpiProcess(data, signals):
     
     option = Option()
 
-    for i in range(data):
-        print(f"Simpi is running: {i}, Signals: {signals[0]}")
-        option.sleep(1)
+    option.waitUntil(signals, 0)
+
+    # for i in range(data):
+    #     print(f"Simpi is running: {i}, Signals: {signals[0]}")
+    #     option.sleep(1)
+    
 
